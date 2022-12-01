737d655152ab3012c9d7bfe4cf3bdfa687afdb5a
SumanBR321 <71173805+SumanBR321@users.noreply.github.com>
2022-12-01 01:13:28 -0800

    Update EnemyController.js

--- EnemyController.js
+++ EnemyController.js
@@ -137,7 +137,7 @@ export default class EnemyController {
       row.forEach((enemyNubmer, enemyIndex) => {
         if (enemyNubmer > 0) {
           this.enemyRows[rowIndex].push(
-            new Enemy(enemyIndex * 1000, rowIndex * 280, enemyNubmer)
+            new Enemy(enemyIndex * 240, rowIndex * 210, enemyNubmer)
           );
         }
       });
